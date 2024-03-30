f14e42f5cd689932b9b4a1f761e6654ede7c9e42
Enrico Bolzonello <enribolzonello@gmail.com>
2024-03-30 19:14:51 +0100

    jenkins

--- Jenkinsfile
+++ Jenkinsfile
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('Build') {
             steps {
-                sh 'cd webapp && npm install'
+                sh 'cd webapp && npm install --production'
             }
         }
         stage('Test') {
--- webapp/jenkins/deliver.sh
+++ webapp/jenkins/deliver.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-npm install --production
-pm2 restart all
+PM2_EXIST=$(if pm2 list 2> /dev/null | grep -q tsp_webapp; then echo "Yes" ; else echo "No" ; fi)
+
+if [ $PM2_EXIST = Yes ] ; then
+    pm2 restart tsp_webapp
+    echo "Restart tsp_webapp."
+else
+    pm2 start file.js --name tsp_webapp
+    echo "Started tsp_webapp."
+fi
